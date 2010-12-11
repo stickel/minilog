6a37d8318579712e7bd210be9ae5f9a93270c74a
Mike Stickel <stickel@eugene.local>
2010-12-10 22:52:01 -0800

    updated sitemap to pull proper post records, fixed post_url params as well

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,6 @@
 class SitemapController < ApplicationController
   def index
-    @posts = Post.published.limited_set(50000)
+    @posts = Post.published.recent(50000)
     respond_to do |wants|
       wants.xml { render :layout => false }
     end
