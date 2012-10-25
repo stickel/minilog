a439f3f71f172dbd5b83bbc182057e5ded27b1aa
stickel <mike@screenflicker.com>
2012-10-24 23:12:05 -0700

    refactor post specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,10 +6,9 @@ describe Post do
     FactoryGirl.build(:post, body: nil).should_not be_valid
   end
 
-  it 'has a unique slug' do
-    post = FactoryGirl.create(:post)
-    post_2 = FactoryGirl.create(:post)
-    post.url.should_not eq(post_2.url)
+  it 'sets the title to the first line of body when title is empty' do
+    post = FactoryGirl.create(:post, title: nil)
+    post.title.should eq(truncate(post.body, length: 55, separator: ' '))
   end
 
   it 'makes a slug from the title' do
@@ -17,14 +16,10 @@ describe Post do
     post.url.should eq(post.title.to_url)
   end
 
-  it 'sets the title to the first line of body when title is empty' do
-    post = FactoryGirl.create(:post, title: nil)
-    post.title.should eq(truncate(post.body, length: 55, separator: ' '))
+  it 'has a unique slug' do
+    post = FactoryGirl.create(:post)
+    post_2 = FactoryGirl.create(:post)
+    post.url.should_not eq(post_2.url)
   end
 
-  context 'logged out user' do
-  end
-
-  context 'logged in user' do
-  end
 end
