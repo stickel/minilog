1770cc6163ec5d2571881da1aa6b85485c9f1c82
stickel <mike@screenflicker.com>
2012-11-26 12:58:17 -0800

    remove view/helper Test::Unit specs from generators

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module Minilog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't generate Test::Unit for views and helpers
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
   end
 end
