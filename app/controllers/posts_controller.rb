2ba334fcf3e4bf1bb56a9a9c5382c88c4447235e
stickel <mike@screenflicker.com>
2012-11-26 13:31:15 -0800

    add Post controller methods and specs (#home, #index, #new, #create, #update, #edit, #show)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,58 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, except: [:home, :index, :show]
+
+  def home
+    @posts = Post.published.recent(10)
+  end
+
+  def index
+    @posts = Post.published.recent(10)
+  end
+
+  def show
+    @post = Post.find_by_url(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    respond_to do |wants|
+      if @post.save
+        wants.html { redirect_to edit_post_path(@post), notice: t('flash.actions.post.create.notice') }
+        wants.json { render json: @post, notice: t('flash.actions.post.create.notice') }
+      else
+        wants.html { render :new, error: t('flash.actions.post.create.error') }
+        wants.json { render json: @post, error: t('flash.actions.post.create.error') }
+      end
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    respond_to do |wants|
+      if @post.update_attributes(params[:post])
+        wants.html { redirect_to edit_post_path(@post.id), notice: t('flash.actions.post.update.notice') }
+        wants.json { render json: @post, notice: t('flash.actions.post.update.notice') }
+      else
+        wants.html { render :edit, error: t('flash.actions.post.update.error') }
+        wants.json { render json: @post, error: t('flash.actions.post.update.error') }
+      end
+    end
+  end
+
+  def destroy
+    if Post.find(params[:id]).delete_it!
+      flash[:notice] = "Post marked as deleted"
+    else
+      flash[:alert] = "Post could not be deleted"
+    end
+    redirect_to :back
+  end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,227 @@
 require 'spec_helper'
 
 describe PostsController do
+  render_views
 
+  describe 'GET #home' do
+    context 'with no posts' do
+      before(:each) do
+        get :home
+      end
+
+      it 'renders the home template' do
+        response.should render_template :home
+      end
+
+      it 'returns empty array' do
+        assigns(:posts).should be_empty
+      end
+
+      it 'shows "sorry" message' do
+        response.body.should have_content I18n.t('posts.home.no_posts')
+      end
+    end
+
+    context 'with posts' do
+      before(:each) do
+        @posts = create_list :post, 12, :published
+        get :home
+      end
+
+      it 'renders the :home template' do
+        response.should render_template 'home'
+      end
+
+      it 'shows the most recent posts' do
+        @posts.sort! { |a, b| a.published_at <=> b.published_at }
+        assigns(:posts).all.should =~ @posts[(@posts.length - 10)..@posts.length].reverse
+      end
+
+      it 'shows the correct number of posts' do
+        assigns(:posts).count.should eq(10)
+      end
+    end
+
+    context 'when logged in' do
+      before(:each) do
+        user = FactoryGirl.create(:user)
+        sign_in user
+        get :home
+      end
+
+      it 'has a current_user' do
+        subject.current_user.should_not be_nil
+      end
+
+      it 'shows "new post" link' do
+        response.body.should have_link 'New post'
+      end
+    end
+  end
+
+  describe 'GET #index' do
+    context 'with posts' do
+      before(:each) do
+        @posts = create_list :post, 12, :published
+        get :index
+      end
+
+      it 'shows the N most recent posts' do
+        @posts.sort! {|a, b| a.published_at <=> b.published_at }
+        assigns(:posts).all.should =~ @posts[(@posts.length - 10)..@posts.length].reverse
+      end
+
+      it 'renders the :index template' do
+        response.should render_template :index
+      end
+    end
+
+    context 'when logged in' do
+      before(:each) do
+        user = FactoryGirl.create(:user)
+        sign_in user
+        get :index
+      end
+
+      it 'has a current_user' do
+        subject.current_user.should_not be_nil
+      end
+
+      it 'shows "new post" link' do
+        response.body.should have_link 'New post'
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    context 'when not logged in' do
+      it 'redirects to Posts#index' do
+        get :new
+        response.should redirect_to new_user_session_path()
+      end
+    end
+
+    context 'when signed in' do
+      before(:each) do
+        user = FactoryGirl.create(:user)
+        sign_in user
+        get :new
+      end
+
+      it 'creates a new post object' do
+        assigns(:post).should be_new_record
+      end
+
+      it 'renders :new template' do
+        response.should render_template :new
+      end
+
+      it 'renders post/form partial' do
+        response.should render_template '_form'
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    before(:each) do
+      user = FactoryGirl.create(:user)
+      sign_in user
+    end
+
+    context 'with valid attributes' do
+      it 'saves the post to the database' do
+        expect {
+          post :create, post: FactoryGirl.attributes_for(:post)
+        }.to change(Post, :count).by(1)
+      end
+
+      it 'redirects to the :edit template' do
+        post :create, post: FactoryGirl.attributes_for(:post)
+        response.should redirect_to edit_post_path(Post.last)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the post to the database' do
+        expect {
+          post :create, post: FactoryGirl.attributes_for(:invalid_post)
+        }.to_not change(Post, :count)
+      end
+
+      it 're-renders the :new template' do
+        post :create, post: FactoryGirl.attributes_for(:invalid_post)
+        response.should render_template :new
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    before(:each) do
+      @post = FactoryGirl.create(:published_post)
+      get :show, id: @post
+    end
+
+    it 'assigns the requested post to @post' do
+      assigns(:post).should eq(@post)
+    end
+
+    it 'renders the :show template' do
+      response.should render_template :show
+    end
+  end
+
+  describe 'GET #edit' do
+    before(:each) do
+      sign_in FactoryGirl.create(:user)
+      @post = FactoryGirl.create(:post)
+      get :edit, id: @post.id
+    end
+
+    it 'renders :edit template' do
+      response.should render_template :edit
+      response.should render_template '_form'
+    end
+
+    it 'returns correct object' do
+      assigns(:post).should eq(@post)
+    end
+  end
+
+  describe 'PUT #update' do
+    before(:each) do
+      sign_in FactoryGirl.create(:user)
+      @post = FactoryGirl.create(:post, title: 'Update this post')
+    end
+
+    context 'valid attributes' do
+      it 'finds the requested post' do
+        put :update, id: @post.id, post: FactoryGirl.attributes_for(:post)
+        assigns(:post).should eq(@post)
+      end
+
+      it 'changes attributes for @post' do
+        put :update, id: @post.id, post: FactoryGirl.attributes_for(:post, title: 'A new title')
+        @post.reload
+        @post.title.should eq('A new title')
+      end
+
+      it 'redirects to the :edit template' do
+        put :update, id: @post.id, post: FactoryGirl.attributes_for(:post)
+        response.should redirect_to edit_post_path(@post.id)
+      end
+    end
+
+    context 'invalid attributes' do
+      it 'does not update the @post' do
+        put :update, id: @post.id, post: FactoryGirl.attributes_for(:post, title: 'A new title', body: '')
+        @post.reload
+        @post.title.should eq('Update this post')
+      end
+
+      it 're-renders the :edit template' do
+        put :update, id: @post.id, post: FactoryGirl.attributes_for(:invalid_post)
+        response.should render_template :edit
+      end
+    end
+  end
 end
