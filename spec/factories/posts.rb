e3fe2763fcb6e9c36d967e88b9d70c998637c57d
stickel <mike@screenflicker.com>
2012-11-26 12:59:28 -0800

    add Post and User factories

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,7 +2,21 @@
 
 FactoryGirl.define do
   factory :post do
-    title "Example Post Title"
+    sequence (:title) { |n| "Example post title #{n}" }
     body "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+    trait :published do
+      # Randomized published_at date so we can test ordering
+      from = Time.local(2009, 1, 1)
+      sequence (:published_at) { Time.at(from + Kernel.rand * (Time.zone.now.to_f - from.to_f)) }
+      state '1'
+    end
+
+    trait :invalid do
+      body ""
+    end
+
+    factory :invalid_post, :traits => [:invalid]
+    factory :published_post, :traits => [:published]
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
+    email "user@test.com"
+    password "password"
+    password_confirmation "password"
   end
 end
